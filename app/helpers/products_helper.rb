8743d0bf13145dbd08c1f4e4050418cb7612f536
montch <montch@gmail.com>
2016-06-16 16:43:18 -0400

    dd uses status

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
 
   def products_for_select
-    Product.all.collect { |m| [m.product_name, m.id] }
+    Product.where(status: 'active').collect { |m| [m.product_name, m.id] }
   end
 end
